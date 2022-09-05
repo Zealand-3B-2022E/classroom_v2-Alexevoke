ac8e997009764add172788b532fb48a0e8e716f6
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-09-05 13:44:55 +0200

    Added ClassRoom class

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class ClassRoom
+    {
+
+
+        public ClassRoom()
+        {
+            ClassName = "dummy";
+            ClassList = new List<Student>();
+            SemesterStart = new DateTime(2022, 9, 1);
+
+        }
+
+
+        public string ClassName { get; set; }
+        public List<Student> ClassList { get; set; }
+        public DateTime SemesterStart { get; set; }
+
+    }
+}
