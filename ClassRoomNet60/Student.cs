bc97c7617d548a89d95961b7c68a42a6a2960025
Alexander Gerdil <alex650w@edu.zealand.dk>
2022-09-05 13:19:55 +0200

    Added Student class

---
+++ ClassRoomNet60/Student.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    public class Student
+    {
+
+        public Student(string name, int birthmonth, int birthday)
+        {
+            Name = name;
+            BirthMonth = birthmonth;
+            BirthDay = birthday;
+        }
+
+
+
+        public string Name { get; private set; }
+        public int BirthMonth { get; private set; }
+        public int BirthDay { get; private set; }
+    }
+}
